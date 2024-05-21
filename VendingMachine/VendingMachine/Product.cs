ba16c45e323f4be13703136688a43a6f13c3f062
Anchu Antony <94461233+AnchuAntony22@users.noreply.github.com>
2024-05-21 16:28:44 +0200

    Update Product.cs
    change void to string

--- VendingMachine/VendingMachine/Product.cs
+++ VendingMachine/VendingMachine/Product.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,31 +24,3 @@ namespace VendingMachine
         public abstract string Use();
     }
 }
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace VendingMachine
-{       public abstract class Product
-    {
-        private static int totalProductCount = 0;
-        public int Id { get; private set; }
-        public string Name { get; set; }
-        public int Price { get; set; }
-
-        public Product(string name, int price)
-        {
-            Id = ++totalProductCount;
-            Name = name;
-            Price = price;
-        }
-
-        public abstract string Examine();
-        public abstract void Use();
-    }
-
-}
->>>>>>> d3b2cb4a9af7b2428e7415940a3e34cbe19022de
