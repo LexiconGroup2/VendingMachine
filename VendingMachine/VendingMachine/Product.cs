b992a0784113ba2e0c165cd9f145367e0f5a3ebd
Olga Te <159808358+olgaTete@users.noreply.github.com>
2024-05-21 15:20:18 +0200

    add Product.cs
    Create an abstract `Product` class with abstract methods `Examine` and `Use`.

---
+++ VendingMachine/VendingMachine/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachine
+{       public abstract class Product
+    {
+        private static int totalProductCount = 0;
+        public int Id { get; private set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+
+        public Product(string name, int price)
+        {
+            Id = ++totalProductCount;
+            Name = name;
+            Price = price;
+        }
+
+        public abstract string Examine();
+        public abstract void Use();
+    }
+
+}
